5861cc8193ffd48b851e7e8e8003da33f534ceb9
Tajmansingh123 <76952196+Tajmansingh123@users.noreply.github.com>
2022-03-27 13:04:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+var bg, bgImg
+var bottomGround
+var topGround
+var balloon, balloonImg
+var obstacleTop, obsTop1, obsTop2
+var obstacleBottom, obsBottom1, obsBottom2, obsBottom3
+
+function preload(){
+bgImg = loadImage("assets/bg.png")
+
+balloonImg = loadAnimation("assets/balloon1.png","assets/balloon2.png","assets/balloon3.png")
+
+obsTop1 = loadImage("assets/obsTop1.png")
+obsTop2 = loadImage("assets/obsTop2.png")
+
+obsBottom1 = loadImage("assets/obsBottom1.png")
+obsBottom2 = loadImage("assets/obsBottom2.png")
+obsBottom3 = loadImage("assets/obsBottom3.png")
+
+}
+
+function setup(){
+
+  createCanvas(400,400)
+//background image
+bg = createSprite(165,485,1,1);
+bg.addImage(bgImg);
+bg.scale = 1.3
+
+
+//creating top and bottom grounds
+bottomGround = createSprite(200,390,800,20);
+bottomGround.visible = false;
+
+topGround = createSprite(200,10,800,20);
+topGround.visible = false;
+      
+//creating balloon     
+balloon = createSprite(100,200,20,50);
+balloon.addAnimation("balloon",balloonImg);
+balloon.scale = 0.2;
+
+
+
+}
+
+function draw() {
+  
+  background("black");
+        
+          //making the hot air balloon jump
+          
+
+          //adding gravity
+          
+   
+        drawSprites();
+       
+        //spawning top obstacles
+      spawnObstaclesTop();
+
+      
+}
+
+
+
+
+ function Bar() 
+ {
+         if(World.frameCount % 60 === 0)
+         {
+           var bar = createSprite(400,200,10,800);
+          bar.velocityX = -6
+          bar.depth = balloon.depth;
+          bar.lifetime = 70;
+          bar.visible = false;
+         }
+}
+
+
+  
